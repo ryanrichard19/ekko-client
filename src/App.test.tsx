aa3f39c730767f06bfb684f00c6c007d9c28171f
Ryan Richard <mrrarichard@gmail.com>
2024-06-19 22:10:06 +0200

    test fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders health status', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = await screen.findByText(/Health Status/i);
   expect(linkElement).toBeInTheDocument();
 });
